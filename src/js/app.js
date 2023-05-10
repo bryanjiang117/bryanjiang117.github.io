c64d06fe1d0302d1a638d0fda1b4816e5778cd04
bryanjiang117 <66335098+bryanjiang117@users.noreply.github.com>
2023-05-10 13:38:21 -0400

    Included both left and right animations
    added hidden-left and hidden-right

--- src/js/app.js
+++ src/js/app.js
@@ -9,5 +9,5 @@ const observer = new IntersectionObserver((entries) => {
   });
 });
 
-const hiddenElements = document.querySelectorAll(".hidden");
+const hiddenElements = document.querySelectorAll(".hidden-left, .hidden-right");
 hiddenElements.forEach((el) => observer.observe(el));

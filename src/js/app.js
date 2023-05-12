a36d6d8fe2029b78088d88ef50f5cbeb7ed56948
bryanjiang117 <66335098+bryanjiang117@users.noreply.github.com>
2023-05-12 00:27:08 -0400

    Added scrolling function that doesn't work and changed threshold
    Tried to change navbar color when scrolling to a new page but I can't get the distance scrolled unless I remove all overflow settings. I need overflow: scroll for the page snap so I'll just comment this out for later. 
    Changed threshold so 99% needs to be seen for an element to be observed.

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,34 @@
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    console.log(entry);
-    if (entry.isIntersecting) {
-      entry.target.classList.add("show");
-    } else {
-      entry.target.classList.remove("show");
-    }
-  });
-});
-
-const hiddenElements = document.querySelectorAll(".hidden-left, .hidden-right");
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      console.log(entry);
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+      } else {
+        entry.target.classList.remove("show");
+      }
+    });
+  },
+  {
+    threshold: 0.99,
+  }
+);
+const hiddenElements = document.querySelectorAll(
+  ".hidden-left, .hidden-left-sm, .hidden-right, .hidden-right-sm"
+);
 hiddenElements.forEach((el) => observer.observe(el));
+
+// const navbar = document.getElementById("navbar");
+// window.addEventListener(
+//   "scroll",
+//   function () {
+//     var scroll = self.pageYOffset;
+//     var height = $(window).height();
+//     if (scroll < height) {
+//       navbar.style.backgroundColor = darkNavy;
+//     } else {
+//       navbar.style.backgroundColor = gray;
+//     }
+//   },
+//   true
+// );

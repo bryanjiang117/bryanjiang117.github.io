2173f9d56ee800a03af03fc0062fb00ba24bcaa0
bryanjiang117 <66335098+bryanjiang117@users.noreply.github.com>
2023-05-13 01:37:52 -0400

    Developing card animations
    Moving cards based on its current position in the stack of cards

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,9 @@
+var root = document.querySelector(":root");
+var styles = getComputedStyle(root);
+var darkNavy = styles.getPropertyValue("--d-navy");
+var aqua = styles.getPropertyValue("--aqua");
+var gray = styles.getPropertyValue("--gray");
+
 const observer = new IntersectionObserver(
   (entries) => {
     entries.forEach((entry) => {
@@ -17,6 +23,33 @@ const hiddenElements = document.querySelectorAll(
   ".hidden-left, .hidden-left-sm, .hidden-right, .hidden-right-sm"
 );
 hiddenElements.forEach((el) => observer.observe(el));
+
+function moveCard() {
+  
+  var cards = document.querySelectorAll('.card');
+  for (let i = 0; i < 3; i++) {
+    const card = cards[i];
+    const classList = card.classList;
+    if (classList.contains("card-back")) {
+      classList.remove("card-back");
+      classList.add("card-forward");
+    } else if (classList.contains("card-forward")) {
+      classList.remove("card-forward");
+      classList.add("card-front");
+    } else if (classList.contains("card-front")) {
+      classList.remove("card-front");
+      classList.add("card-back");
+    } else if (card.id == "card1") {
+      classList.add("card-back");
+    } else if (card.id == "card2") {
+      classList.add("card-front");
+    } else if (card.id == "card3") {
+      classList.add("card-forward");
+    } else {
+      alert("error");
+    }
+  }
+}
 
 // const navbar = document.getElementById("navbar");
 // window.addEventListener(
